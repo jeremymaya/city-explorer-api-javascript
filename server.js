82173407a8c83f11261d667c4c4779ba0d6f5c22
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-10-15 22:51:06 -0700

    basic server created and README updated

--- server.js
+++ server.js
@@ -1,49 +1,16 @@
 'use strict';
 
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+// Load Environment Variables from the .env file
 require('dotenv').config();
 
+// Application dependencies
+const express = require('express');
+const cors = require('cors');
+
+// Application setup
+const PORT = process.env.PORT;
+const app = express();
 app.use(cors());
 
-app.get('/location', (request, response) =>{
-  let searchQuery = request.query.data;
-  const geoData = require('./data/geo.json');
-
-  const location = new Location(searchQuery, geoData);
-
-  response.status(200).send(location);
-})
-
-app.get('/weather', (request, response) => {
-  const darkskyData = require('./data/darksky.json');
-  const tempArr = [];
-  
-  darkskyData.daily.data.forEach(obj => {
-    let tempVar = new Weather(obj);
-    tempArr.push(tempVar);
-  })
-
-  response.status(200).send(tempArr);
-})
-
-function Location(searchQuery, geoData){
-  this.search_query = searchQuery;
-  this.formatted_query = geoData.results[0].formatted_address;
-  this.latitude = geoData.results[0].geometry.location.lat;
-  this.longitude = geoData.results[0].geometry.location.lng;
-}
-
-function Weather(obj){
-  this.forecast = obj.summary;
-  this.time = this.formattedDate(obj.time);
-}
-
-Weather.prototype.formattedDate = function(time) {
-  let date = new Date(time*1000);
-  return date.toDateString();
-}
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
