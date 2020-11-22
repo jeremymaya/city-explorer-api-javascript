89eca92db0ca0bcbe840ecc18bf80b097fb71523
Kyungrae <kyungraekim@outlook.com>
2020-11-21 17:51:21 -0800

    lab 08 todo 2 complete

--- server.js
+++ server.js
@@ -3,14 +3,19 @@
 // Load Environment Variables from the .env file
 require('dotenv').config();
 
-// Application dependencies
+// Dependencies
 const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
 const cors = require('cors');
 
 // Postgres client setup
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 // Application setup
 const PORT = process.env.PORT || 3000;
@@ -21,14 +26,13 @@ app.use(cors());
 app.get('/', getIndex);
 app.get('/location', getLocation);
 app.get('/weather', getWeather);
-
 app.use('*', catchAll);
 
 // Handler function for the GET /location route
-// Returns an object which contains the necessary information for correct client rendering
+// Returns an object containing city information from the database
 function getLocation(request, response) {
     const city = request.query.city;
-    const sql = 'SELECT * FROM locations WHERE search_query=$1';
+    const sql = `SELECT * FROM locations WHERE search_query=$1`;
 
     client
         .query(sql, [city])
@@ -36,6 +40,8 @@ function getLocation(request, response) {
         .catch(error => handleInternalError(error));
 }
 
+// Search for a city not in the database
+// Returns an object containing city information from LocationIQ API
 function searchLocation(request, response) {
     const city = request.query.city;
     const url = 'https://us1.locationiq.com/v1/search.php';
@@ -58,6 +64,7 @@ function searchLocation(request, response) {
         .catch(error => handleInternalError(error));
 }
 
+// Saves the new city infromation to the database
 function saveLocation(location) {
     const sql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);';
     const value = [location.search_query, location.formatted_query, location.latitude, location.longitude];
@@ -71,8 +78,8 @@ function saveLocation(location) {
 function Location(city, geoData) {
     this.search_query = city;
     this.formatted_query = geoData.display_name;
-    this.latitude = geoData.lat;
-    this.longitude = geoData.lon;
+    this.lat = geoData.lat;
+    this.lon = geoData.lon;
 }
 
 // Handler function for the GET /weather route
@@ -82,8 +89,8 @@ function getWeather(request, response) {
     const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
     const parameters = {
         key: process.env.WEATHER_API_KEY,
-        lat: city.latitude,
-        lon: city.longitude
+        lat: city.lat,
+        lon: city.lon
     };
 
     superagent
@@ -94,9 +101,7 @@ function getWeather(request, response) {
             const forecast = weatherData.map(weather => new Weather(weather)); 
             response.status(200).send(forecast);
         })
-        .catch(error => {
-            handleInternalError(error);
-        });
+        .catch(error => handleInternalError(error));
 }
 
 // A constructor function that converts an object to a weather object
@@ -111,18 +116,24 @@ Weather.prototype.formattedDate = function(valid_date) {
     return date.toDateString();
 }
 
+// Handler function for the GET / route
 function getIndex(request, response) {
     response.status(200).send('Pair this backend with: https://codefellows.github.io/code-301-guide/curriculum/city-explorer-app/front-end');
 }
 
+// Handler function for internal errors
 function handleInternalError(error) {
     console.log('ERROR', error);
     response.status(500).send('Sorry, something went wrong');
 }
 
+// Handler function for all other errors
 function catchAll(request, response) {
     response.status(404).send('404 Not Found D:');
 }
 
-// Make sure the server is listening for requests
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+// Connect to Postgres then start the server
+client
+    .connect()
+    .then(() => app.listen(PORT, () => console.log(`Endpoint: http://localhost:${PORT}`)))
+    .catch(error => handleInternalError(error));
